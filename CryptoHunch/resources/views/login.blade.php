c73b58dbeaba275a1f454a00e2196a4587a641dd
Michael <mobrienoct1981@gmail.com>
2021-12-09 13:29:09 +0100

    mytemplate blade added

--- CryptoHunch/resources/views/login.blade.php
+++ CryptoHunch/resources/views/login.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('mytemplate')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>Login</title>
+@section('title', 'Login')
 
-</head>
 @section('content')
 
 @if ($message = Session::post('success'))
