9ebaef31a51d94339efefd61ea3375953f66254c
Kasia <katarzyna27@gmail.com>
2021-12-09 14:53:44 +0100

    test branch

---
+++ CryptoHunch/app/Http/Controllers/BlogController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function addBlogpost(){
+
+    }
+
+
+
+    public function viewBlogpost(){
+       
+    }
+
+
+
+    public function favouriteBlogpost(){
+        
+    }
+
+
+
+    public function viewFavouriteBlogpost(){
+        
+    }
+
+
+    public function deleteTip(){
+        
+    }
+
+}
---
+++ CryptoHunch/app/Http/Controllers/TipController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use 
+
+class TipController extends Controller
+{
+    public function giveTip(){
+
+    }
+
+
+
+    public function viewTip(){
+       
+    }
+
+
+
+    public function favouriteTip(){
+        
+    }
+
+
+
+    public function viewFavouriteTip(){
+        
+    }
+
+
+
+    public function viewTipStats(){
+        
+    }
+
+
+
+
+    public function deleteTip(){
+        
+    }
+
+
+}
